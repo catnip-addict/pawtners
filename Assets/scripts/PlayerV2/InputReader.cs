cfa12b0babd78ee0fab20aec33c6ef430b5b35e8
Maciek Matysiak <125466831+ItsMaciek@users.noreply.github.com>
2025-03-10 22:28:47 +0100

    Reworked movment script and fixed some shit

---
+++ Assets/scripts/PlayerV2/InputReader.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static Input_Actions;
+public enum PlayerControllerType
+{
+    Keyboard,
+    Gamepad
+}
+namespace Platformer
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Platformer/InputReader")]
+    public class InputReader : ScriptableObject, IKeyboardActions, IControllerActions
+    {
+        public PlayerControllerType playerControllerType;
+        public event UnityAction<Vector2> Move = delegate { };
+        public event UnityAction<Vector2, bool> Look = delegate { };
+        public event UnityAction EnableMouseControlCamera = delegate { };
+        public event UnityAction DisableMouseControlCamera = delegate { };
+        public event UnityAction<bool> Jump = delegate { };
+
+        Input_Actions inputActions;
+
+        public Vector3 Direction
+        {
+            get
+            {
+                if (playerControllerType == PlayerControllerType.Keyboard)
+                {
+                    return inputActions.Keyboard.Walking.ReadValue<Vector2>();
+                }
+                else
+                {
+                    return inputActions.Controller.Walking.ReadValue<Vector2>();
+                }
+            }
+        }
+
+        void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new Input_Actions();
+                if (playerControllerType == PlayerControllerType.Keyboard)
+                {
+                    inputActions.Keyboard.SetCallbacks(this);
+                }
+                else
+                {
+                    inputActions.Controller.SetCallbacks(this);
+                }
+            }
+        }
+
+        public void EnablePlayerActions()
+        {
+            inputActions.Enable();
+        }
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            Look.Invoke(context.ReadValue<Vector2>(), IsDeviceMouse(context));
+        }
+
+        bool IsDeviceMouse(InputAction.CallbackContext context) => context.control.device.name == "Mouse";
+
+        public void OnFire(InputAction.CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnMouseControlCamera(InputAction.CallbackContext context)
+        {
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    EnableMouseControlCamera.Invoke();
+                    break;
+                case InputActionPhase.Canceled:
+                    DisableMouseControlCamera.Invoke();
+                    break;
+            }
+        }
+
+        public void OnRun(InputAction.CallbackContext context)
+        {
+            // noop
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    Jump.Invoke(true);
+
+                    break;
+                case InputActionPhase.Canceled:
+                    Jump.Invoke(false);
+                    break;
+
+            }
+
+        }
+
+        public void OnWalking(InputAction.CallbackContext context)
+        {
+            Move.Invoke(context.ReadValue<Vector2>());
+        }
+    }
+}
