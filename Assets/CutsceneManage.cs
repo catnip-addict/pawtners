f3231f5a27b43c1f5eab396ccd7ef900e4e078c0
Zwierzu2115 <162510318+Zwierzu2115@users.noreply.github.com>
2025-04-27 15:10:18 +0200

    prawie cutscena akt 1

---
+++ Assets/CutsceneManage.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public class CutsceneManage : MonoBehaviour
+{
+    public GameObject cutsceneObject;
+    public Animator cutsceneAnim;
+    public Camera playerCam1;
+    public Camera playerCam2;
+    public Camera cutsceneCam;
+    public Player playerScript;
+    public string cutsceneTriggerName;
+    public float animationTime;
+
+    private bool player1Ready = false;
+    private bool player2Ready = false;
+
+  
+    public void PlayerEntered(int playerID)
+    {
+
+        if (playerID == 1)
+            player1Ready = true;
+        else if (playerID == 2)
+            player2Ready = true;
+
+        CheckIfStartCutscene();
+    }
+
+    private void CheckIfStartCutscene()
+    {
+        if (player1Ready && player2Ready)
+        {
+            StartCoroutine(StartCutscene());
+        }
+    }
+
+    private IEnumerator StartCutscene()
+    {
+
+        yield return new WaitForSeconds(0.5f);  
+
+        playerScript.enabled = false;
+        cutsceneObject.SetActive(true);
+        cutsceneCam.gameObject.SetActive(true);
+        playerCam1.gameObject.SetActive(false);
+        playerCam2.gameObject.SetActive(false);
+
+        cutsceneAnim.SetTrigger(cutsceneTriggerName);
+
+        yield return new WaitForSeconds(animationTime);
+
+        playerScript.enabled = true;
+        playerCam1.gameObject.SetActive(true);
+        playerCam2.gameObject.SetActive(true);
+        cutsceneCam.gameObject.SetActive(false);
+        cutsceneObject.SetActive(false);
+    }
+}
