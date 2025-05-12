8573a8939d85c2d902c4f4ec00dc8a648b8fa5a3
Maciek Matysiak <125466831+ItsMaciek@users.noreply.github.com>
2025-05-12 12:34:05 +0200

    yrest

--- Assets/scripts/Akt2/BuildingShip.cs
+++ Assets/scripts/Akt2/BuildingShip.cs
@@ -5,7 +5,9 @@ public class BuildingShip : MonoBehaviour
 {
     [SerializeField] private List<Renderer> shipParts;
     [SerializeField] private List<kloda> PutInShipParts;
+    [SerializeField] private Animator animator;
     private Material WoodMaterial;
+    bool isOnIsland = false;
     public int currentPartIndex = 0;
 
     void OnTriggerEnter(Collider other)
@@ -22,9 +24,32 @@ public class BuildingShip : MonoBehaviour
             // Destroy(other.gameObject);
             other.gameObject.SetActive(false);
         }
+        if (other.CompareTag("Island"))
+        {
+            isOnIsland = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Island"))
+        {
+            isOnIsland = false;
+        }
+    }
+    public void SwimToIsland()
+    {
         if (currentPartIndex >= shipParts.Count)
         {
-
+            if (isOnIsland)
+            {
+                animator.SetBool("SwimTo", false);
+                animator.SetBool("SwimBack", true);
+            }
+            else
+            {
+                animator.SetBool("SwimTo", true);
+                animator.SetBool("SwimBack", false);
+            }
         }
     }
 }
